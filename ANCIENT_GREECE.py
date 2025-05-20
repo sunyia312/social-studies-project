dac43eabf9dee2934d699b11e4b4bdee90b46b01
yian sun number 0 <yzhh77@hotmail.com>
2025-05-20 08:36:57 -0700

    update on weekends

--- ANCIENT_GREECE.py
+++ ANCIENT_GREECE.py
@@ -1,16 +1,13 @@
-from time import sleep
 from random import randint
 import tkinter as tk
 import json
 
 
-start_game = 0
-num_to_role = {1: "Citizen", 2: "Metic", 3: "Slave"}
-
+num_to_role = {1: "Citizen", 2: "Woman", 3: "Slave"}
+counter = 0
 
 
 def get_entry(the_entry, root, label, button):
-    global start_game
     val = the_entry.get()
     label.config(text=f"You chose: {num_to_role[int(val)]}!", padx=100, pady=100)
     the_entry.destroy()
@@ -18,19 +15,50 @@ def get_entry(the_entry, root, label, button):
     root.update()
     root.after(2000)
     label.config(text=f"Put scenario here", padx=100, pady=20)
-    game_loop(label, root)
+    game_loop(label, root, int(val))
+
+def clear_fors1(c1, c2, label, data, sc, root, role):
+    c1.destroy()
+    c2.destroy()
+    label.config(text=data[sc]["choices"][0][1])
+    proceed = tk.Button(root, text="OK", command=lambda: next_scenario(label, proceed, root, role))
+    proceed.grid(row=1, column=0)
+
+def clear_fors2(c1, c2, label, data, sc, root, role):
+    c1.destroy()
+    c2.destroy()
+    label.config(text=data[sc]["choices"][1][1])
+    proceed = tk.Button(root, text="OK", command=lambda: next_scenario(label, proceed, root, role))
+    proceed.grid(row=1, column=0)
+
+def next_scenario(label, proceed, root, role):
+    proceed.destroy()
+    game_loop(label, root, role)
+
+
     
 
 
-def game_loop(label, root):
-    scenario = randint(0, 1)
-    with open("huh.json") as abc:
-        data = json.load(abc)
+def game_loop(label, root, role):
+    global counter
+    scenario = counter
+    counter += 1
+    if role == 1:
+        with open("citizen.json") as abc:
+            data = json.load(abc)
+    elif role == 2:
+        with open("metic.json") as xyz:
+            data = json.load(xyz)
+    elif role == 3:
+        with open("slave.json") as efg:
+            data = json.load(efg)
     label.grid(row=0, column=0, columnspan=2, sticky="n")
     label.config(text=data[scenario]["text"])
     print(data[scenario]["choices"][0][0])
     choice1 = tk.Button(root, text=data[scenario]["choices"][0][0])
     choice2 = tk.Button(root, text=data[scenario]["choices"][1][0])
+    choice1.config(command=lambda: clear_fors1(choice1, choice2, label, data, scenario, root, role))
+    choice2.config(command=lambda: clear_fors2(choice1, choice2, label, data, scenario, root, role))
     choice1.grid(row=1, column=0)
     choice2.grid(row=1, column=1)
 
